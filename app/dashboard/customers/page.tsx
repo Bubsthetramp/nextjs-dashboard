029f152bd00de662ec000e6f4e50bbd4f4ea36dd
bubbles <urmas.ploom123@mail.ee>
2023-12-18 11:24:27 -0400

    Version 1

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+const page = () => {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  );
+};
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+const page = () => {
+  return (
+    <div>
+      <p>Invoices Page</p>
+    </div>
+  );
+};
+export default page;
